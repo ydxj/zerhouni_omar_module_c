5e391b33f662922a3aba70423c1b1ba2d41ef74e
Zerhouni Omar <hddjdndjdndj@gmail.com>
2026-02-12 22:05:00 +0100

    Create the updateGameBySlug that use to update the description, title in the GameController

--- app/Controllers/GameController.php
+++ app/Controllers/GameController.php
@@ -9,6 +9,38 @@ class GameController {
         return $db->execute([$slug]);
     }
 
+    public function updateGameBySlug($slug, $title, $description, $authorId) {
+        $model = new Model();
+        $db = $model->getDb();
+        
+        $db->query("SELECT author_id FROM game WHERE slug = ?");
+        $game = $db->execute([$slug]);
+        
+        if ($game[0]['author_id'] != $authorId) {
+            http_response_code(403);
+            return [
+                'status' => 'forbidden', 
+                'message' => 'You are not the game author'
+            ];
+        }
+        
+        $db->query("UPDATE game SET title = ?, description = ? WHERE slug = ?");
+        $responce = $db->execute([$title, $description, $slug]);
+        if($responce) {
+            http_response_code(200);
+            return [
+                'status' => 'success', 
+                'message' => 'Game updated succesfully'
+            ];
+        }
+        
+        http_response_code(500);
+        return [
+            'status' => 'error', 
+            'message' => 'An error occurred while updating the game'
+        ];
+    }
+
     public function getAllGames() { 
         $model = new Model(); 
         $db = $model->getDb(); 
